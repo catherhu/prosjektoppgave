266294857e1ac6a10f90c45d0494a8e8a51f3374
Catherine Hughes <catherhu@uio.no>
2024-10-07 11:46:10 +0200

    hydrogen atom

--- 3d_hydrogen.py
+++ 3d_hydrogen.py
@@ -18,19 +18,18 @@ dP_dx = np.polynomial.legendre.legder(P) # derivative coefficients
 x = np.zeros(N + 1) # grid points
 x[0] = -1 
 x[N] = 1
-x [1: -1] = np.polynomial.legendre.legroots(dP_dx) # polynomial derivative roots
+x[1: -1] = np.polynomial.legendre.legroots(dP_dx) # polynomial derivative roots
 P_x = np.polynomial.legendre.legval(x[1: -1], P) # evaluating polynomial at grid points
 
-r = L*(1 + x)/(1 - x + alpha) # radial grid points
+r = L*(1 + x[1: -1])/(1 - x[1: -1] + alpha) # radial grid points
 
 # potential:
-V = np.zeros(N + 1)
-V[1: -1] = -1/r[1: -1] 
+V = -1/r
 
 # set up matrix:
-
 M = np.zeros((N + 1, N + 1))
-d = V + l*(l + 1)/(2*r) # values on the diagonal
+d = np.zeros(N + 1)
+d[1: -1] = V + l*(l + 1)/(2*r) # values on the diagonal
 np.fill_diagonal(M, d)
 
 dr_dx = L*(2 + alpha)/(1 - x + alpha)**2
@@ -44,28 +43,37 @@ for i in range(1, N):
             D[i][j] = -2/(x[i] - x[j])**2/(dr_dx[i]*dr_dx[j])
 
 M += -0.5*D
-M = M[1: -1, 1: -1]
 
 # solve:
-E, eigf = LA.eigh(M)
+E, eigf = LA.eigh(M[1: -1, 1: -1])
+
 f = eigf.T * P_x
-"""
-go back to original function psi
-make 1D plots
-make contour plots
-"""
+u = f / np.sqrt(dr_dx[1: -1])
+R = u / r
+Y = 1 # add later
+psi = R * Y
 
-# plot:
+# plot radial function:
+plt.figure(1)
+plt.grid()
+for i in range (3):
+    R_i = R[i]
+    plt.plot(r, -R_i, label = f"{i + 1}{l}")
+plt.legend()
+plt.savefig("3d_hydrogen_radial.png")
+
+# contour plot:
+plt.figure(2)
 phi = np.linspace(1, 2*np.pi, N - 1)
-x_points = r[1: -1]*np.cos(phi)
-y_points = r[1: -1]*np.sin(phi)
-X, Y = np.meshgrid(x_points, y_points)
-psi_0 = psi[0]/np.sqrt(X**2 + Y**2)
-plt.contour(X, Y, psi_0**2, levels=10, cmap='viridis')
+x_points = r * np.cos(phi)
+y_points = r * np.sin(phi)
+x_mesh, y_mesh = np.meshgrid(x_points, y_points)
+psi_0 = psi[0]/np.sqrt(x_mesh**2 + y_mesh**2)
+plt.contour(x_mesh, y_mesh, psi_0**2, levels=10, cmap='viridis')
 plt.colorbar()
-plt.savefig("3d_hydrogen.png")
+plt.savefig("3d_hydrogen_contour.png")
 
-print(E)
+#print(E)
 
 
 
