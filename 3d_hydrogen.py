042cd558dafdc4788927e06aa231863a51daf9b9
Catherine Hughes <catherhu@uio.no>
2024-10-23 14:58:28 +0200

    backup

--- 3d_hydrogen.py
+++ 3d_hydrogen.py
@@ -1,79 +1,114 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
-from scipy import integrate
+from scipy.special import sph_harm
+from scipy.special import laguerre
+import plotly.graph_objects as go
+from scipy.interpolate import CubicSpline
+from math import factorial
+import os
 
 # constants:
-r_max = 10 # radial boundaries
-l = 0 # angular momentum
+r_max = 20 # radial boundaries
 L = 1 # mapping constant
 alpha = 2*L/r_max # mapping constant
 
-# grid:
-N = 100 # polynomial degree
-P = np.zeros(N + 1) # polynomial coefficients
-P[-1] = 1 
-dP_dx = np.polynomial.legendre.legder(P) # derivative coefficients
+N = 50
 
-x = np.zeros(N + 1) # grid points
-x[0] = -1 
-x[N] = 1
-x[1: -1] = np.polynomial.legendre.legroots(dP_dx) # polynomial derivative roots
-P_x = np.polynomial.legendre.legval(x[1: -1], P) # evaluating polynomial at grid points
 
-r = L*(1 + x[1: -1])/(1 - x[1: -1] + alpha) # radial grid points
+def setup_grid(N):
 
-# potential:
-V = -1/r
+    P = np.zeros(N + 1) # polynomial coefficients
+    P[-1] = 1 
+    dP_dx = np.polynomial.legendre.legder(P) # derivative coefficients
 
-# set up matrix:
-M = np.zeros((N + 1, N + 1))
-d = np.zeros(N + 1)
-d[1: -1] = V + l*(l + 1)/(2*r) # values on the diagonal
-np.fill_diagonal(M, d)
+    x = np.zeros(N + 1) # grid points
+    x[0] = -1 
+    x[N] = 1
+    x[1: -1] = np.polynomial.legendre.legroots(dP_dx) # polynomial derivative roots
 
-dr_dx = L*(2 + alpha)/(1 - x + alpha)**2
-D = np.zeros((N + 1, N + 1))
+    P_x = np.polynomial.legendre.legval(x, P) # evaluating polynomial at grid points
 
-for i in range(1, N):
-    for j in range(1, N):
-        if i == j:
-            D[i][j] = (-1/3)*N*(N + 1)/(1 - x[i]**2)/dr_dx[i]**2
-        else:
-            D[i][j] = -2/(x[i] - x[j])**2/(dr_dx[i]*dr_dx[j])
+    r = L*(1 + x)/(1 - x + alpha) # radial grid points
+    dr_dx = L*(2 + alpha)/(1 - x + alpha)**2
 
-M += -0.5*D
+    return(x, r, dr_dx, P_x)
 
-# solve:
-E, eigf = LA.eigh(M[1: -1, 1: -1])
 
-f = eigf.T * P_x
-u = f / np.sqrt(dr_dx[1: -1])
-R = u / r
-Y = 1 # add later
-psi = R * Y
+def setup_matrix(N, l):
 
-# plot radial function:
-plt.figure(1)
-plt.grid()
-for i in range (3):
-    R_i = R[i]
-    plt.plot(r, -R_i, label = f"{i + 1}{l}")
-plt.legend()
-plt.savefig("3d_hydrogen_radial.png")
+    x, r, dr_dx, P_x = setup_grid(N)
+    V = -1/r[1: -1]
 
-# contour plot:
-plt.figure(2)
-phi = np.linspace(1, 2*np.pi, N - 1)
-x_points = r * np.cos(phi)
-y_points = r * np.sin(phi)
-x_mesh, y_mesh = np.meshgrid(x_points, y_points)
-psi_0 = psi[0]/np.sqrt(x_mesh**2 + y_mesh**2)
-plt.contour(x_mesh, y_mesh, psi_0**2, levels=10, cmap='viridis')
-plt.colorbar()
-plt.savefig("3d_hydrogen_contour.png")
+    M = np.zeros((N + 1, N + 1))
+    d = np.zeros(N + 1)
+    d[1: -1] = V + l*(l + 1)/(2*r[1: -1]) # values on the diagonal
+    np.fill_diagonal(M, d)
 
-#print(E)
+    D = np.zeros((N + 1, N + 1))
+
+    for i in range(1, N):
+        for j in range(1, N):
+            if i == j:
+                D[i][j] = (-1/3)*N*(N + 1)/(1 - x[i]**2)/dr_dx[i]**2
+            else:
+                D[i][j] = -2/(x[i] - x[j])**2/(dr_dx[i]*dr_dx[j])
+
+    M += -0.5*D
+
+    return(M[1: -1, 1: -1], r, dr_dx, P_x)
+
+
+def radial(N, l):
+
+    M, r, dr_dx, P_x = setup_matrix(N, l)
+
+    E, eigf = LA.eigh(M)
+    f = eigf.T * P_x[1: -1]
+    u = f / np.sqrt(dr_dx[1: -1])
+
+    R = u / r[1: -1]
+    integral = np.sum(2 / (N * (N + 1) * P_x[1: -1] ** 2) * f**2)
+    R_norm = R / np.sqrt(integral)
+
+    return(r, R_norm)
+
+
+def radial_analytical(l, n, r_max):
+
+    a0 = 0.529
+    r = np.linspace(0, r_max, 100)
+
+    norm_const = np.sqrt(((2 / (n * a0)) ** 3 * factorial(n - l - 1)) / (2 * n * factorial(n + l) ** 3))
+    laguerre_pol = laguerre(n - l - 1, 2 * l + 1)(2 * r / (n * a0))
+    R = norm_const * np.exp(-r / (n * a0)) * (2 * r / (n * a0))**l * laguerre_pol
+
+    return(r, R)
+
+def plot_radial(r_max, n_max):
+    
+    l_list = list(range(n_max))
+
+    for l in l_list:
+        r, R = radial(N, l)
+
+        for i in range(l, n_max):
+
+            n = i + 1
+            R_n = R[i]
+            plt.plot(r[1: -1], -R_n, label = f"{n}{l}")
+
+            r_an, R_n_an = radial_analytical(l, n, r_max)
+            plt.plot(r_an, R_n_an, label = f"{n}{l} analytical")
+
+            plt.xlim(0, r_max)
+            plt.ylim(-0.02, 0.15)
+            plt.grid()
+            plt.legend()
+            plt.savefig("3d_hydrogen_radial.png")
+
+
+plot_radial(r_max, n_max = 3)
 
 
 
